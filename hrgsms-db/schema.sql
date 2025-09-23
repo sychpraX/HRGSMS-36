94357f41c938978feb1b69ce7f5cf0aa6d6f7435
Praveen Nawarathna <tpn.142@gmail.com>
2025-09-23 18:37:59 +0530

    Relational integrity is added

--- hrgsms-db/schema.sql
+++ hrgsms-db/schema.sql
@@ -2,14 +2,21 @@ USE hrgsms_db;
 
 CREATE TABLE IF NOT EXISTS Branch (
 	branchID BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    branchLocation VARCHAR(20) NOT NULL,
+  branchLocation VARCHAR(20) NOT NULL,
 	rating NUMERIC(2,1),
-    phone char(12) NOT NULL,
-    email varchar(30) NOT NULL,
-    CONSTRAINT chk_phone CHECK(
-		phone like "+94%"),
+  phone char(12) NOT NULL,
+  email varchar(30) NOT NULL,
+  CONSTRAINT chk_phone CHECK(
+	phone like '+94%'),
 	CONSTRAINT chk_rating CHECK(
-		rating >=0 AND rating <= 5)
+	rating >=0 AND rating <= 5)
+);
+
+CREATE TABLE IF NOT EXISTS Room_Type (
+	typeID	INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	typeName VARCHAR(20) NOT NULL,
+	capacity INT NOT NULL,
+	currRate NUMERIC(10,2) NOT NULL
 );
     
 CREATE TABLE IF NOT EXISTS Room (
@@ -17,23 +24,27 @@ CREATE TABLE IF NOT EXISTS Room (
 	branchID BIGINT UNSIGNED NOT NULL,
 	typeID	INT UNSIGNED NOT NULL,
 	roomNo	INT UNSIGNED NOT NULL,
-	roomStatus	ENUM("Occupied", "Available") NOT NULL,
+	roomStatus	ENUM('Occupied', 'Available') NOT NULL,
+  CONSTRAINT fk_branchID 
+    FOREIGN KEY (branchID) REFERENCES Branch(branchID) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_typeID 
+    FOREIGN KEY (typeID) REFERENCES Room_Type(typeID)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  UNIQUE KEY uq_branch_roomNo (branchID, roomNo), 
+  CONSTRAINT chk_roomNo CHECK(roomNo>0)
 );
 
-CREATE TABLE IF NOT EXISTS Room_Type (
-	typeID	INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	typeName VARCHAR(20) NOT NULL,
-	capacity INT NOT NULL,
-	rate NUMERIC(10,2) NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS Guest (
   guestID     BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   firstName   VARCHAR(50) NOT NULL,
   lastName    VARCHAR(50) NOT NULL,
-  phone       VARCHAR(20),
+  phone       VARCHAR(20) NOT NULL,
   email       VARCHAR(100),
-  idNumber    VARCHAR(30),          
+  idNumber    VARCHAR(30) UNIQUE NOT NULL,
+  CONSTRAINT chk_phone CHECK(
+	phone like '+94%')      
 );
 
 CREATE TABLE IF NOT EXISTS Booking (
@@ -41,61 +52,58 @@ CREATE TABLE IF NOT EXISTS Booking (
   guestID       BIGINT UNSIGNED NOT NULL,
   branchID      BIGINT UNSIGNED NOT NULL,
   roomID        BIGINT UNSIGNED NOT NULL,
+  rate          NUMERIC(10,2) UNSIGNED NOT NULL, -- Snapshot of the rate at the time of booking
   checkInDate   DATETIME NOT NULL,
   checkOutDate  DATETIME NOT NULL,
-  numGuests     INT NOT NULL,
+  numGuests     INT NOT NULL UNSIGNED,
   bookingStatus ENUM('Booked','CheckedIn','CheckedOut','Cancelled') NOT NULL DEFAULT 'Booked',
+  CONSTRAINT fk_guestID 
+    FOREIGN KEY (guestID) REFERENCES Guest(guestID)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_branchID 
+    FOREIGN KEY (branchID) REFERENCES Branch(branchID)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_roomID 
+    FOREIGN KEY (roomID) REFERENCES Room(roomID)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT chk_date CHECK(checkInDate<=checkOutDate)
 );
 
-CREATE TABLE IF NOT EXISTS Chargable_Service (
+CREATE TABLE IF NOT EXISTS Chargeble_Service (
   serviceID   INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  serviceType ENUM( "Spa services", "Pool access", "room service", "laundry", "minibar usage") NOT NULL, 
-  unit ENUM("per person","per kg", "per item", "per request"), 
-  ratePerUnit  DECIMAL(10,2) NOT NULL
+  serviceType ENUM( 'Spa services', 'Pool access', 'room service', 'laundry', 'minibar usage') NOT NULL, 
+  unit ENUM('per person','per kg', 'per item', 'per request'), 
+  ratePerUnit  DECIMAL(10,2) NOT NULL UNSIGNED
 );
 
 CREATE TABLE IF NOT EXISTS Service_Usage ( 
   usageID	BIGINT UNSIGNED PRIMARY KEY NOT NULL,
   bookingID  BIGINT UNSIGNED NOT NULL,
   serviceID  INT UNSIGNED NOT NULL,
-  quantity   INT NOT NULL DEFAULT 1,
-  usedAt	TIMESTAMP NOT NULL
+  rate  DECIMAL(10,2) NOT NULL UNSIGNED,     -- Snapshot of the rate at the time of service usage
+  quantity   INT NOT NULL UNSIGNED DEFAULT 1,
+  usedAt	TIMESTAMP NOT NULL,
+  CONSTRAINT fk_bookingID 
+    FOREIGN KEY (bookingID) REFERENCES Booking(bookingID)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_serviceID 
+    FOREIGN KEY (serviceID) REFERENCES Chargeble_Service(serviceID)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Late_Checkout (
-	lateID BIGINT UNSIGNED PRIMARY KEY NOT NULL,
-	bookingID BIGINT UNSIGNED NOT NULL,
+	bookingID BIGINT UNSIGNED PRIMARY KEY NOT NULL,
 	checkOutTime TIMESTAMP,
-	amount NUMERIC(10,2)
-);
+	amount NUMERIC(10,2) UNSIGNED,
+  CONSTRAINT fk_bookingID 
+    FOREIGN KEY (bookingID) REFERENCES Booking(bookingID)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 
-CREATE TABLE IF NOT EXISTS Invoice (
-  invoiceID       BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  bookingID       BIGINT UNSIGNED NOT NULL,
-  policyID        INT UNSIGNED NULL,         
-  discountCode    INT UNSIGNED NULL,         
-  paymentPlan     ENUM('Full','Installment') NOT NULL DEFAULT 'Full',
-  roomCharges     DECIMAL(15,2) NOT NULL DEFAULT 0.00,
-  serviceCharges  DECIMAL(15,2) NOT NULL DEFAULT 0.00,
-  totalAmount     DECIMAL(15,2) NOT NULL DEFAULT 0.00,
-  taxAmount       DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-  discountAmount  DECIMAL(10,2) NOT NULL DEFAULT 0.00,
-  finalAmount     DECIMAL(15,2) NOT NULL DEFAULT 0.00,   
-  settledAmount   DECIMAL(15,2) NOT NULL DEFAULT 0.00,   
-  invoiceStatus          ENUM('Pending','Partially Paid','Paid','Cancelled') NOT NULL DEFAULT 'Pending',
 );
-
-CREATE TABLE IF NOT EXISTS Payment (
-  transactionID   BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  invoiceID       BIGINT UNSIGNED NOT NULL,
-  transactionDate DATE NOT NULL,
-  paymentMethod   ENUM('Cash','Card','Online','Other') NOT NULL,
-  amount          DECIMAL(15,2) NOT NULL
-) ;
 
 CREATE TABLE IF NOT EXISTS Tax_Policy (
   policyID   INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  rate       DECIMAL(5,4) NOT NULL,       
+  rate       DECIMAL(5,4) UNSIGNED NOT NULL,       
   appliesTo  VARCHAR(20) NOT NULL,        
   policyName       VARCHAR(100) NOT NULL        
 );
@@ -104,15 +112,52 @@ CREATE TABLE IF NOT EXISTS Discount (
   discountCode  INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   discountName  VARCHAR(100) NOT NULL,            
   condition     VARCHAR(200),                     
-  discountType  ENUM('Percent','Fixed') NOT NULL, 
-  discountValue   DECIMAL(5,2) NOT NULL,            
+  discountValue   DECIMAL(5,2) UNSIGNED NOT NULL,            
   validFrom     DATE NOT NULL,
   validTo       DATE NOT NULL
+  CONSTRAINT chk_dateValidity CHECK(validTo>validFrom)
 ) ;
+
+CREATE TABLE IF NOT EXISTS Invoice (
+  invoiceID       BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  bookingID       BIGINT UNSIGNED NOT NULL UNIQUE,
+  policyID        INT UNSIGNED NULL,         
+  discountCode    INT UNSIGNED NULL,         
+  paymentPlan     ENUM('Full','Installment') NOT NULL DEFAULT 'Full',
+  roomCharges     DECIMAL(15,2) NOT NULL UNSIGNED DEFAULT 0.00,    --Must be saved becasue the rates can change daily but once offered rates cannot change
+  serviceCharges  DECIMAL(15,2) NOT NULL UNSIGNED DEFAULT 0.00, 
+  taxAmount       DECIMAL(10,2) NOT Null UNSIGNED DEFAULT 0.00,
+  discountAmount  DECIMAL(10,2) NOT NULL UNSIGNED DEFAULT 0.00,
+  settledAmount   DECIMAL(15,2) NOT NULL UNSIGNED DEFAULT 0.00,   
+  invoiceStatus   ENUM('Pending','Partially Paid','Paid','Cancelled') NOT NULL DEFAULT 'Pending',
+  CONSTRAINT fk_bookingID 
+    FOREIGN KEY (bookingID) REFERENCES Booking(bookingID)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_policyID 
+    FOREIGN KEY (policyID) REFERENCES Tax_Policy(policyID)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT fk_discountCode 
+    FOREIGN KEY (discountCode) REFERENCES Discount(discountCode)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT chk_paidAmount CHECK(settledAmount<=(roomCharges+serviceCharges+taxAmount-discountAmount))
+
+);
+
+CREATE TABLE IF NOT EXISTS Payment (
+  transactionID   BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  invoiceID       BIGINT UNSIGNED NOT NULL,
+  transactionDate DATE NOT NULL,
+  paymentMethod   ENUM('Cash','Card','Online','Other') NOT NULL,
+  amount          DECIMAL(15,2) UNSIGNED NOT NULL,
+  CONSTRAINT fk_invoiceID 
+    FOREIGN KEY (invoiceID) REFERENCES Invoice(invoiceID)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+) ;
+
 
 CREATE TABLE IF NOT EXISTS User_Account (
   userID      INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  branchID    INT UNSIGNED NULL,
+  branchID    BIGINT UNSIGNED NULL,
   username    VARCHAR(100) NOT NULL UNIQUE,
   userPassword    VARCHAR(255) NOT NULL,             
   userRole        ENUM('Admin','Manager','Reception','Staff') NOT NULL,
@@ -121,16 +166,28 @@ CREATE TABLE IF NOT EXISTS User_Account (
   last_name   VARCHAR(20) NOT NULL,
   phone       VARCHAR(15),
   email       VARCHAR(30) UNIQUE,
+  CONSTRAINT fk_branchID 
+    FOREIGN KEY (branchID) REFERENCES Branch(branchID)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 ) ;
 
 CREATE TABLE IF NOT EXISTS Log (
   logID        BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   event_time   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  branchID     INT UNSIGNED NULL,
+  branchID     BIGINT UNSIGNED NULL,
   userID       INT UNSIGNED NULL,
   bookingID    BIGINT UNSIGNED NULL,
   logAction       ENUM('Create','Update','Delete','CheckIn','CheckOut','Payment','Other') NOT NULL,
   logDescription  VARCHAR(255),
+  CONSTRAINT fk_branchID 
+    FOREIGN KEY (branchID) REFERENCES Branch(branchID)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT fk_userID 
+    FOREIGN KEY (userID) REFERENCES User_Account(userID)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT fk_bookingID 
+    FOREIGN KEY (bookingID) REFERENCES Booking(bookingID)
+    ON DELETE SET NULL ON UPDATE CASCADE
 ) ;
 
 
